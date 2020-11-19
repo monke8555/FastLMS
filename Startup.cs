d7c9e3086ac605dd2d79a9cf3087daabfba93ec9
omerhijazi404 <omerhijazi@outlook.com>
2020-11-19 13:01:26 +0300

    Added 404 page and cookie consent
    Added a page for handling 404 status codes (Pages/404.cshtml). Created cookie consent banner in Pages/Shared/_Layout.cshtml so it's appleid to all pages. Displayed only if user hasn't clicked the "I agree" button (saved in a cookie)

--- Startup.cs
+++ Startup.cs
@@ -1,13 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ExaminerApp
 {
@@ -39,6 +34,16 @@ namespace ExaminerApp
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.Use(async (context, next) =>
+            {
+                await next();
+                if (context.Response.StatusCode == 404)
+                {
+                    context.Request.Path = "/404";
+                    await next();
+                }
+            });
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
