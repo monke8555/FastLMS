30bd05f78c943e032720490df0ade35c12a0ee81
omerhijazi404 <omerhijazi@outlook.com>
2020-11-22 13:54:09 +0300

    Made better cookie consent, added cookie information, and Google Analytics and Adsense tags

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+function openTab(evt, tabName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("tab");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+        tablinks[i].classList.remove("w3-light-grey");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.classList.add("w3-light-grey");
+}
